79bac50fbb030e32753ee1d386bdecc371377436
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2014-09-18 13:21:31 +0200

    Initial commit

---
+++ weather_dht.ino
@@ -0,0 +1,38 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT22_PIN 2
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  int chk = DHT.read22(DHT22_PIN);
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+		Serial.print("{\"sensor\":\"DHT22\",\"type\":\"temperature\",\"value\":");
+                Serial.print(DHT.temperature);
+                Serial.println("}");                
+		Serial.print("{\"sensor\":\"DHT22\",\"type\":\"humidity\",\"value\":");
+                Serial.print(DHT.humidity);
+                Serial.println("}");
+		break;
+    case DHTLIB_ERROR_CHECKSUM: 
+		//Serial.print('Checksum error,\t'); 
+		break;
+    case DHTLIB_ERROR_TIMEOUT: 
+		//Serial.print("Time out error,\t"); 
+		break;
+    default: 
+		//Serial.print("Unknown error,\t"); 
+		break;
+  }
+
+  delay(3000);
+}
+
